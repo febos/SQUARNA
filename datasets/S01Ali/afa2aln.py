e98e823d1d23cbb9adbfc1af159101dbe727832e
Eugene Baulin <efbaulin@gmail.com>
2025-01-13 10:09:09 +0100

    tool/data split

--- datasets/S01Ali/afa2aln.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-import glob
-
-for file in glob.glob("afa_ungap/*"):
-    alnfile = file.replace("afa_ungap/","aln_ungap/").replace(".afa",".aln")
-    records = SeqIO.parse(file, "fasta")
-    count = SeqIO.write(records, alnfile, "clustal")
-    print("Converted {}".format(file))
--- datasets/SubAli/afa2aln.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-import glob
-
-for file in glob.glob("afa/*"):
-    alnfile = file.replace("afa/","aln/").replace(".afa",".aln")
-    records = SeqIO.parse(file, "fasta")
-    count = SeqIO.write(records, alnfile, "clustal")
-    print("Converted {}".format(file))
